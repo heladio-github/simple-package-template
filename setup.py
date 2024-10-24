f86bb1216eac61e665616992f2c33844878e195f
heladio-github <heladiopaes13@gmail.com>
2024-10-24 15:14:45 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+adiofrom setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
     page_description = f.read()
@@ -9,9 +9,9 @@ with open("requirements.txt") as f:
 setup(
     name="package_name",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="heladio",
+    author_email="heladiopaes13@gmail.com",
+    description="projeto de criacao de pacote",
     long_description=page_description,
     long_description_content_type="text/markdown",
     url="my_github_repository_project_link"
